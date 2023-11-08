26a750973f5d1d9378d04eb555baa8925d1c9cc1
kevinthomas <kevvin.thomas@gmail.com>
2023-11-08 16:20:53 +0530

    Initial Commit

---
+++ FinlockAssignmentKevin/FinlockAssignmentKevinApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinlockAssignmentKevinApp.swift
+//  FinlockAssignmentKevin
+//
+//  Created by Kevin Thomas on 08/11/23.
+//
+
+import SwiftUI
+
+@main
+struct FinlockAssignmentKevinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
